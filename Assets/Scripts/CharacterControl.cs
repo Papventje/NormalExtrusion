8a9c27474f9cb1b57718e06df1f11c7c97e70caf
Papventje <robbierobsnor@gmail.com>
2017-10-20 15:15:17 +0200

    Commit normal extrusion shader

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+    static Animator animator;
+
+    [Header ("Character :")]
+    public float speed = 2.0f;
+    public float rotationSpeed = 75.0f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if(translation != 0)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
